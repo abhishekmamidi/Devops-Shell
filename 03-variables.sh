7e6b5165cff9abedca39b6b7c412cbbbeb6b646d
Abhishek Mamidi <mamidi.abhishek1@gmail.com>
2026-01-30 17:14:13 +0200

    added a line

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 person1=Trump
 person2=Modi    
-echo"$person1": Hi "$person2"  How are you"
+echo"$person1": Hi "$person2"  How are you
 echo"$person2": I am good and you
 echo"$person1": I am good thanks
